442e2b37f35ff9df2e97994d3c374185c8a0d89d
KSVasquez <ksv3212@rit.edu>
2022-04-22 00:42:15 -0400

    initial commit

---
+++ js/counter.js
@@ -0,0 +1,70 @@
+let minusBtn = document.querySelector("#minus-btn");
+let count = document.querySelector("#count");
+let plusBtn = document.querySelector("#plus-btn");
+let custom = document.querySelector("#custom");
+let bar = document.querySelector(".bar");
+let over = document.querySelector(".over");
+let fs = document.querySelector("#fsbutton");
+
+let startNum = 150;
+let countNum = 150;
+let customNum = 1;
+count.innerHTML = countNum;
+
+custom.addEventListener("change", (e) => {
+    customNum = Number(e.target.value);
+});
+custom.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
+        custom.blur();
+    }
+});
+
+minusBtn.addEventListener("click", () => {
+    countNum -= customNum;
+    countNum = Math.max(countNum, 0);
+    count.innerHTML = countNum;
+    custom.value = "1";
+    customNum = 1;
+    updateBar();
+});
+plusBtn.addEventListener("click", () => {
+    countNum += customNum;
+    count.innerHTML = countNum;
+    custom.value = "1";
+    customNum = 1;
+    updateBar();
+});
+
+fs.addEventListener("click", () => {
+    if (document.fullscreenElement) {
+        document.exitFullscreen();
+    } else {
+        document.documentElement.requestFullscreen();
+    }
+});
+document.documentElement.addEventListener("fullscreenchange", () => {
+    if (document.fullscreenElement) {
+        fs.innerHTML = '<i class="fa-solid fa-compress"></i>';
+    } else {
+        fs.innerHTML = '<i class="fa-solid fa-expand"></i>';
+    }
+});
+
+updateBar();
+
+function updateBar() {
+    let old = parseFloat(bar.style.width) + parseFloat(over.style.width);
+
+    let percent = countNum / startNum * 100;
+    let overPer = Math.min(Math.max(percent - 100, 0), 100);
+    let newPer = Math.min(Math.max(percent, 0), 100);
+
+    let change = Math.abs(percent - old);
+    let time = 0.2 + change / 50;
+    bar.style.setProperty('--barAnimTime', time + "s");
+    over.style.setProperty('--barAnimTime', (0.2 + time) + "s");
+
+    bar.style.width = newPer + "%"
+    over.style.width = overPer + "%"
+}
